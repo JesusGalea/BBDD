a4d44773fcc0ac374feb6cf6fb3ca3ddb1868357
JesusGalea <jrgaleasantiago98@gmail.com>
2022-03-07 09:35:45 +0000

    ../docker-compose.yml index.php stylesheet.css

---
+++ Practica3/www/index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<title></title>
+</head>
+<body>
+<div class="banner"> </div>
+<div class="menu"> 
+<div class="iz">
+
+</div>
+
+
+
+<div class="der">
+	mi polla derecha
+
+</div>
+<div class="barra">
+	mi polla en barra
+
+</div>
+
+<div class="content"> mi polla contenida</div>
+
+</div>
+
+
+</body>
+</html>
